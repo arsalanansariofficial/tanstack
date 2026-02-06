0bfd1cf055c2a6192438f0ed1d639a3abca40dc9
Arsalan Ansari <arsalanansariofficial@outlook.com>
2026-02-06 10:00:00 +0530

    Add draggable row for sorting rows according to priority

--- src/table/table.tsx
+++ src/table/table.tsx
@@ -1,19 +1,110 @@
+import { useId, useMemo, useState } from 'react';
+import {
+  SortableContext,
+  useSortable,
+  verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
+import { restrictToVerticalAxis } from '@dnd-kit/modifiers';
+import { CSS } from '@dnd-kit/utilities';
+
 import {
   flexRender,
-  getCoreRowModel,
   useReactTable,
+  getCoreRowModel,
+  type Row,
 } from '@tanstack/react-table';
 
-export type Person = {
-  firstName: string;
-  lastName: string;
-  age: number;
-  visits: number;
-  progress: number;
-};
+import {
+  useSensor,
+  DndContext,
+  useSensors,
+  MouseSensor,
+  TouchSensor,
+  closestCenter,
+  KeyboardSensor,
+  type DragEndEvent,
+  type UniqueIdentifier,
+} from '@dnd-kit/core';
+
+export function DraggableRow<T extends { id: number }>(props: { row: Row<T> }) {
+  const { row } = props;
+  const {
+    isDragging,
+    setNodeRef,
+    transform,
+    transition,
+    attributes,
+    listeners,
+  } = useSortable({
+    id: row.original.id,
+  });
+
+  return (
+    <tr
+      {...listeners}
+      {...attributes}
+      ref={setNodeRef}
+      data-dragging={isDragging}
+      data-state={row.getIsSelected() && 'selected'}
+      style={{ transform: CSS.Transform.toString(transform), transition }}
+      className="relative z-0 data-[dragging=true]:z-10 data-[dragging=true]:opacity-80 border"
+    >
+      {row.getAllCells().map(cell => (
+        <td key={cell.id} className="text-center">
+          {flexRender(cell.column.columnDef.cell, cell.getContext())}
+        </td>
+      ))}
+    </tr>
+  );
+}
 
 export default function App() {
+  const sortableId = useId();
+
+  const sensors = useSensors(
+    useSensor(MouseSensor, {}),
+    useSensor(TouchSensor, {}),
+    useSensor(KeyboardSensor, {}),
+  );
+
+  const [data, setData] = useState([
+    {
+      id: 1,
+      firstName: 'Gwen',
+      lastName: 'Tennyson',
+      age: 25,
+      visits: 1,
+      progress: 25,
+      priority: 1,
+    },
+    {
+      id: 2,
+      firstName: 'Kevin',
+      lastName: 'Eleven',
+      age: 27,
+      visits: 2,
+      progress: 30,
+      priority: 2,
+    },
+    {
+      id: 3,
+      firstName: 'Ben',
+      lastName: 'Tennyson',
+      age: 27,
+      visits: 2,
+      progress: 30,
+      priority: 3,
+    },
+  ]);
+
+  const dataIds = useMemo<UniqueIdentifier[]>(
+    () => data.map(({ id }) => id),
+    [data],
+  );
+
   const table = useReactTable({
+    data,
+    getCoreRowModel: getCoreRowModel(),
     columns: [
       {
         header: 'First Name',
@@ -36,64 +127,117 @@ export default function App() {
         accessorKey: 'progress',
         cell: info => info.getValue(),
       },
-    ],
-    data: [
       {
-        firstName: 'Gwen',
-        lastName: 'Tennyson',
-        age: 25,
-        visits: 1,
-        progress: 25,
-      },
-      {
-        firstName: 'Kevin',
-        lastName: 'Eleven',
-        age: 27,
-        visits: 2,
-        progress: 30,
-      },
-      {
-        firstName: 'Ben',
-        lastName: 'Tennyson',
-        age: 27,
-        visits: 2,
-        progress: 30,
+        header: 'Priority',
+        accessorKey: 'priority',
+        cell: info => info.getValue(),
       },
     ],
-    getCoreRowModel: getCoreRowModel(),
   });
+
+  function handleDragEnd(event: DragEndEvent) {
+    const { active, over } = event;
+
+    const from = data.find(d => d.id === active.id);
+    const to = data.find(d => d.id === over?.id);
+
+    if (!active || !over || !from || !to || active.id === over.id) return;
+
+    if (to.priority < from.priority) {
+      return setData(data => {
+        const sorted = data
+          .sort((a, b) => a.priority - b.priority)
+          .map(d => {
+            if (
+              d.priority !== from.priority &&
+              d.priority >= to.priority &&
+              d.priority !== data.length
+            ) {
+              return { ...d, priority: d.priority + 1 };
+            }
+
+            return d;
+          })
+          .map(d => {
+            if (d.id === from.id) {
+              return { ...d, priority: to.priority };
+            }
+
+            return d;
+          })
+          .sort((a, b) => a.priority - b.priority);
+
+        console.log(sorted);
+        return sorted;
+      });
+    }
+
+    return setData(data => {
+      const sorted = data
+        .sort((a, b) => a.priority - b.priority)
+        .map(d => {
+          if (
+            d.priority !== from.priority &&
+            d.priority <= to.priority &&
+            d.priority > 1
+          ) {
+            return { ...d, priority: d.priority - 1 };
+          }
+
+          return d;
+        })
+        .map(d => {
+          if (d.id === from.id) {
+            return { ...d, priority: to.priority };
+          }
+
+          return d;
+        })
+        .sort((a, b) => a.priority - b.priority);
+
+      console.log(sorted);
+      return sorted;
+    });
+  }
 
   return (
     <main className="min-h-screen content-center justify-items-center">
-      <table>
-        <thead>
-          {table.getHeaderGroups().map(headerGroup => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map(header => (
-                <th key={header.id} className="p-1 border">
-                  {header.isPlaceholder
-                    ? null
-                    : flexRender(
-                        header.column.columnDef.header,
-                        header.getContext(),
-                      )}
-                </th>
+      <DndContext
+        id={sortableId}
+        sensors={sensors}
+        onDragEnd={handleDragEnd}
+        collisionDetection={closestCenter}
+        modifiers={[restrictToVerticalAxis]}
+      >
+        <table>
+          <thead>
+            {table.getHeaderGroups().map(headerGroup => (
+              <tr key={headerGroup.id} className="border">
+                {headerGroup.headers.map(header => (
+                  <th key={header.id} className="p-1">
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </th>
+                ))}
+              </tr>
+            ))}
+          </thead>
+          <tbody>
+            <SortableContext
+              items={dataIds}
+              strategy={verticalListSortingStrategy}
+            >
+              {table.getRowModel().rows.map(row => (
+                <DraggableRow key={row.id} row={row} />
               ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {table.getRowModel().rows.map(row => (
-            <tr key={row.id}>
-              {row.getAllCells().map(cell => (
-                <td key={cell.id} className="p-1 border text-center">
-                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+            </SortableContext>
+          </tbody>
+        </table>
+      </DndContext>
     </main>
   );
 }
